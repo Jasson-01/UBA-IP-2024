59d34335c65e40082e3fb0116b1d96883a4d9710
Jasson-01 <jason.davila001@gmail.com>
2024-05-28 01:03:14 -0300

    avanzando con el ejercicio 6 de la guia 6

--- Guias/GUIA 6 - Introducción al lenguaje Imperativo (Python)/Ejercicio5.py
+++ Guias/GUIA 6 - Introducción al lenguaje Imperativo (Python)/Ejercicio5.py
@@ -17,7 +17,8 @@ def devolver_valor_si_es_par_sino_el_que_sigue(numero:int)->int:
 print(devolver_valor_si_es_par_sino_el_que_sigue(5))
 
 
-# ---  EJM 2  ---
+# ---  EJM 3  ---
+
 def devolver_el_doble_si_es_multiplo3_el_triple_si_es_multiplo9v1(numero)->int:
      if numero % 3 == 0:
           return 2*numero
@@ -26,19 +27,49 @@ def devolver_el_doble_si_es_multiplo3_el_triple_si_es_multiplo9v1(numero)->int:
      else:
           return numero
 
+print(devolver_el_doble_si_es_multiplo3_el_triple_si_es_multiplo9v1(18))
+
+# ---  EJM 4  ---
+
+def lindo_nombre(nombre:str)->str:
+    if len(nombre) >= 5:
+         return "Tu nombre tiene muchas letras!"
+    return "Tu nombre tiene menos de 5 caracteres"
+
+print(lindo_nombre("Marjorie"))
 
 
+# ---  EJM 5  ---
 
+def rango(numero:int):
+    if numero < 5:
+       return "Menor a 5"   
+    if numero >= 10 and numero <= 20:
+       return "Entre 10 y 20"
+    if numero > 20:
+        return "Mayor a 20"
+    return numero
 
+print(rango(21))
 
+# ---  EJM 6  ---
 
-
-
-
-
-
-
-
+def jubilacion(genero:str,edad:int)->str:
+    if genero == "F" or "f":
+       if edad >= 18 and edad < 60:
+           return "Te toca trabajar"
+       if edad >= 60 :
+           return "Andá de vacaciones"
+       else:
+           return "Andá de vacaciones"
+    if genero == "M" or "m":
+       if edad >= 18 and edad < 65:
+           return "Te toca trabajar"
+       if edad >= 65 :
+           return "Andá de vacaciones"
+       else:
+           return "Andá de vacaciones"
+print(jubilacion("f",16))
 
 
 
---
+++ Guias/GUIA 6 - Introducción al lenguaje Imperativo (Python)/Ejercicio6.py
@@ -0,0 +1,131 @@
+#  Implementar las siguientes funciones usando repetición condicional while:
+
+# --- EJM 1 ---
+
+def numeros_1_al_10():
+    numero = 0
+    while numero <= 10 :
+        print(numero)
+        numero += 1
+
+numeros_1_al_10()
+
+# --- EJM 2 ---
+
+def pares_10_al_40():
+    numero = 10
+    while numero <= 40:
+        print(numero)
+        numero += 2
+pares_10_al_40()
+
+# --- EJM 3 ---
+
+def imprimir_10_veces_eco():
+    contador = 1
+    while contador < 11:
+        print(f"{contador}:eco")
+        contador += 1
+imprimir_10_veces_eco()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
