cb6b130c2e656d73948cea43e873a1f3b7830008
Jasson-01 <jason.davila001@gmail.com>
2024-05-30 15:48:44 -0300

    avanzando con el problema 2

--- Guias/GUIA 8 - Archivos-Pilas-Colas-Diccionarios/Ejercicio1.py
+++ Guias/GUIA 8 - Archivos-Pilas-Colas-Diccionarios/Ejercicio1.py
@@ -12,13 +12,30 @@ def contar_Lineas(lista:list[int])->int:
 
 archivo = open("mi_archivo.txt","r",encoding='utf8')
 contenido : str = archivo.readlines()
-print(contar_Lineas(contenido))
+print((contenido))
 
 # --- EJM 2 ---
+"""
+def existe_palabra(palabra:str,nombre_archivo:str)->bool:
+    archivo = open(nombre_archivo,"r"):
 
-#def existe_palabra(palabra:str,nombre_archivo:str)->bool:
-     
+    for linea in archivo.readlines():
+         listaDePalabras: list[str] = funcionx(linea)
+         if pertenece(palabra,listaDePalabras):
+              return True
+    archivo.close()
+    return False
 
+palabra:str  = "bien"
+print(existe_palabra(palabra,"mi_archivo.txt"))
+"""
+
+# funcionx -> Desaparece los espacion en linea y separa las palabras una por una , loco
+
+
+#archivo2 = open("mi_archivo.txt","r")
+#contenido2 : str = archivo2.read()
+#print(existe_palabra("capo",contenido2))
 
 
 
