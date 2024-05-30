0ff7b945bbf7b57183541d0e372c025f6cc8bb00
Jasson-01 <jason.davila001@gmail.com>
2024-05-30 08:07:20 -0300

    ejercicio 1 de la guia 8 resuelto y modificando la ubicacion del archivo txt

--- Guias/GUIA 8 - Archivos-Pilas-Colas-Diccionarios/Ejercicio1.py
+++ Guias/GUIA 8 - Archivos-Pilas-Colas-Diccionarios/Ejercicio1.py
@@ -1,16 +1,6 @@
-
 # --- EJM 1 ---
-"""
-def contar_lineas(nombreArchivo:str)->int:
-    archivo = open("nombreArchivo","r")
-    archivoTextoarchivo = archivo.readlines()
-    archivo.close()
-    return cantidadLista(archivoTextoarchivo)
 
-print(contar_lineas("mi_archivo.txt"))
-"""
-
-def cantidadLista(lista:list[int])->int:
+def contar_Lineas(lista:list[int])->int:
     contador = 0
     longitud = (len(lista)-1)
     while longitud >= 0:
@@ -20,12 +10,14 @@ def cantidadLista(lista:list[int])->int:
 
 #print(cantidadLista([1,2,3]))
 
+archivo = open("mi_archivo.txt","r",encoding='utf8')
+contenido : str = archivo.readlines()
+print(contar_Lineas(contenido))
 
-archivo = open("mi_archivo.txt","r")
-archivoTextoArchivo = archivo.readlines()
-print(cantidadLista(archivoTextoArchivo))
+# --- EJM 2 ---
 
-
+#def existe_palabra(palabra:str,nombre_archivo:str)->bool:
+     
 
 
 
