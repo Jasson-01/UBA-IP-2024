3708c81cf07ed2ea3c9c432dcaaeb1bd91cf5826
Jasson-01 <jason.davila001@gmail.com>
2024-06-07 01:06:07 -0300

    Problem-1 terminado

---
+++ Parciales/Parcial-4/Solucion-Parcial4.py
@@ -0,0 +1,168 @@
+# -------------- PROBLEM-1 ---------------
+
+def ultima_aparicion(s:list[int],e:int)->int:
+    posicion = int()
+    for i in range(len(s)):
+        if s[i] == e :
+            posicion = i
+    return posicion
+
+#s = [-1,4,0,4,100,0,100,0,-1,-1]
+#e = 0
+#print(ultima_aparicion(s,e))
+# res=7
+
+# -------------- PROBLEM-2 ---------------
+
+#def elementos_exclusivos(s:list[int],t:list[int])->list[int]:
+    
+
+
+
+
+
+
+#   s = [-1,4,0,4,3,0,100,0,-1,-1]
+#   t = [0,100,5,0,100,-1,5]
+
+# res = [3,4,5] ó res = [3,5,4] ó res = [4,3,5] ó
+# res = [4,5,3] ó res = [5,3,4] ó res = [5,4,3]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
