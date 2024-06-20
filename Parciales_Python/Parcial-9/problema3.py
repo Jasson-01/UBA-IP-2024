c691a0fae306f786dfd6aa9ac7c4be8422277813
Json01 <jason.davila001@gmail.com>
2024-06-20 00:31:14 -0300

    Nueva actualizacion V14

--- Parciales_Python/Parcial-9/problema3.py
+++ Parciales_Python/Parcial-9/problema3.py
@@ -1,16 +1,35 @@
 
+def racha_mas_larga(tiempos:list[int])->tuple[int,int]:
+    #Primero formo una copia de la lista original para trabajar con ella 
+    copia_tiempos = tiempos.copy()
+    
+    comienza_indice = 0
+    termina_indice = 0
+    lista_subsecuencias = [] 
+    subsecuencia = []
+    
+   
+    
+    
+    #Recorro la copia
+    for indice in range(len(copia_tiempos)):
+        for num in copia_tiempos:
+             if copia_tiempos[0] != 0 or copia_tiempos[0] != 61:
+                  comienza_indice = 0
+            
+             elif copia_tiempos[i] != 0 or copia_tiempos[i] != 61 :
+                  comienza_indice = i
+                  subsecuencia.append(copia_tiempos[i])         
+        termina_indice = i
+        
+           
+    lista_subsecuencias.append((comienza_indice,termina_indice),subsecuencia) 
 
 
 
-
-
-
-
-
-
-
-
-
+tiempos = [1,22,0,4,3,7,61,20,30,40,50,60,0,14,15]
+print(racha_mas_larga(tiempos))
+#res = (7,11)
 
 
 
