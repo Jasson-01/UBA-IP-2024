30b0aa69766e6803d849f3f432f7430de010c0ff
Json01 <jason.davila001@gmail.com>
2024-06-20 12:43:46 -0300

    problema 3 terminado

--- Parciales_Python/Parcial-9/problema3.py
+++ Parciales_Python/Parcial-9/problema3.py
@@ -1,5 +1,55 @@
+# Hecho por Chat GPT
+def racha_mas_larga(tiempos: list[int]) -> tuple[int, int]:
 
-def racha_mas_larga(tiempos:list[int])->tuple[int,int]:
+    # Primera parte
+    copia_tiempos = tiempos.copy()
+    comienza_indice = 0
+    lista_subsecuencias = []
+    subsecuencia = []
+    indice_actual = 0
+
+    # Recorro la copia
+    while indice_actual < len(copia_tiempos):
+        tiempo = copia_tiempos[indice_actual]
+
+        if tiempo != 0 and tiempo != 61:
+            if len(subsecuencia) == 0:
+                comienza_indice = indice_actual
+            subsecuencia.append(tiempo)
+        else:
+            if len(subsecuencia) > 0:
+                lista_subsecuencias.append(
+                    ((comienza_indice, indice_actual - 1), subsecuencia))
+                subsecuencia = []
+
+        indice_actual += 1
+
+    if len(subsecuencia) > 0:
+        lista_subsecuencias.append(
+            ((comienza_indice, indice_actual - 1), subsecuencia))
+
+#    return lista_subsecuencias
+
+    # 2DA parte
+    # Ahora busco la longitud de la subsecuencia mas grande
+    tupla_res = (0, 0)
+   # maximo = []
+    longitud_maxima = 0
+    for (indices, subsecuencias) in lista_subsecuencias:
+        if longitud_maxima <= len(subsecuencias):
+            #   maximo = lista_subsecuencias[i][1]
+            longitud_maxima = len(subsecuencias)
+            tupla_res = indices
+    return tupla_res
+
+# tiempos1 = [1, 22, 0, 4, 3, 7, 61, 20, 30, 40, 50, 60, 0, 14, 15]
+# tiempos2 = [2, 3, 0, 61, 8, 9, 10]
+# print(racha_mas_larga(tiempos2))
+# print(racha_mas_larga(tiempos1))
+
+
+""" No pasa los test que hice (Primera parte)
+def racha_mas_larga(tiempos:list[int])->list:
     #Primero formo una copia de la lista original para trabajar con ella 
     copia_tiempos = tiempos.copy()
     
@@ -7,43 +57,32 @@ def racha_mas_larga(tiempos:list[int])->tuple[int,int]:
     termina_indice = 0
     lista_subsecuencias = [] 
     subsecuencia = []
-    
-   
-    
-    
-    #Recorro la copia
-    for indice in range(len(copia_tiempos)):
-        for num in copia_tiempos:
-             if copia_tiempos[0] != 0 or copia_tiempos[0] != 61:
-                  comienza_indice = 0
-            
-             elif copia_tiempos[i] != 0 or copia_tiempos[i] != 61 :
-                  comienza_indice = i
-                  subsecuencia.append(copia_tiempos[i])         
-        termina_indice = i
+    indice_manual = -1
+   # indice_anterior = indice_manual - 1 
+      
+    #Recorro la copia    
+    for tiempo in copia_tiempos:
+        if tiempo != 0 and tiempo != 61 : 
+          if len(lista_subsecuencias) == 0 :             
+            comienza_indice = 0
+          subsecuencia.append(tiempo)
+        elif len(subsecuencia) > 0 :
+            termina_indice = indice_manual    
+            lista_subsecuencias.append(((comienza_indice,termina_indice),subsecuencia)) 
+            subsecuencia = []
+            indice_actual = indice_manual + 1   
+            comienza_indice = indice_actual + 1         
+        indice_manual += 1
+    if len(subsecuencia) > 0 :
+        termina_indice = indice_manual
+        lista_subsecuencias.append(((comienza_indice,termina_indice),subsecuencia))    
+      
+        
+    return lista_subsecuencias        
         
            
-    lista_subsecuencias.append((comienza_indice,termina_indice),subsecuencia) 
-
-
-
-tiempos = [1,22,0,4,3,7,61,20,30,40,50,60,0,14,15]
-print(racha_mas_larga(tiempos))
-#res = (7,11)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+tiempos2 = [2,3,0,61,8,9,10]
+print(racha_mas_larga(tiempos2))
+"""
+# res = [((0,1),[2,3]),((4,6),[8,9,10])]
+# tiempos = [1,22,0,4,3,7,61,20,30,40,50,60,0,14,15]
