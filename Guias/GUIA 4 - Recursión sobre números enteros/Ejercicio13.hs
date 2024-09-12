acf5bee439597adfc2734051cec24176d3ed2839
linuxlite <jason.davila001@gmail.com>
2024-09-12 08:58:08 -0300

    Agregando una 2da forma al ejercicio 13 de la guia 4

--- Guias/GUIA 4 - Recursión sobre números enteros/Ejercicio13.hs
+++ Guias/GUIA 4 - Recursión sobre números enteros/Ejercicio13.hs
@@ -1,3 +1,5 @@
+-- 1ERA FORMA
+
 sumatoriaDoble :: Integer -> Integer -> Integer
 sumatoriaDoble n m = aux1 n m m
 
@@ -5,3 +7,13 @@ aux1 :: Integer -> Integer -> Integer -> Integer
 aux1 0 _ _ = 0
 aux1 n m k | n /= 0 && m /= 0 = (n^m) + aux1 n (m-1) k  
            | n /= 0 && m == 0 = aux1 (n-1) k k
+
+-- 2DA FORMA
+
+sumatoriaDoble :: Integer -> Integer -> Integer
+sumatoriaDoble n m = auxSumatoriaDoble n m m
+
+auxSumatoriaDoble :: Integer -> Integer -> Integer -> Integer
+auxSumatoriaDoble n m contador | n == 0 = 0 
+                               | m == 1 = (n^m) + (auxSumatoriaDoble (n-1) (contador) contador) 
+                               | otherwise = (n^m) + (auxSumatoriaDoble n (m-1) contador)
